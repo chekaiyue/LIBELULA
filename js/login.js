d2a9426e750016660fce24e4a68ffc780c8179cc
karin <331785074@qq.com>
2022-03-20 20:06:14 +0800

    串联admin和customer页面

---
+++ js/login.js
@@ -0,0 +1,23 @@
+function login() {
+  var username = document.getElementById("username")
+  var password = document.getElementById("password")
+  console.log(username, password)
+
+  if(username.value == "") {
+    alert("请输入用户名");
+    return false
+  } 
+  else if(password.value == ""){
+    alert("请输入密码");
+    return false
+  }
+  else if(username.value == "admin" && password.value == "admin") {
+    window.location.href = "../adminPage/index.html"
+    return true
+  }
+  else {
+    window.location.href = "../customerPage/index.html"
+    return false
+  }
+
+}
